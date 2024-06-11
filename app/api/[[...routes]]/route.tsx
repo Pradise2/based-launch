72568f87a5a07db2b8c4f1ea6cb02ab709d9b083
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-06-11 13:07:37 +0100

    Initial commit

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,16 +1,17 @@
 /** @jsxImportSource frog/jsx */
 
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
-import { handle } from 'frog/next'
-import { serveStatic } from 'frog/serve-static'
-import { neynar } from 'frog/middlewares'
+import { Button, Frog } from 'frog';
+import { devtools } from 'frog/dev';
+import { handle } from 'frog/next';
+import { serveStatic } from 'frog/serve-static';
+import { neynar } from 'frog/middlewares';
 import dotenv from 'dotenv';
 import 'dotenv/config';
 
 dotenv.config();
 
 const neynarApiKey = process.env.NEYNAR_API_KEY || '';
+const warpcasthash = process.env.WARPCAST_HASH || '';
 
 const app = new Frog({
   assetsPath: '/',
@@ -36,50 +37,30 @@ app.frame('/Join-Waitlist', (c) => {
   const interactor = c.var.interactor;
   const cast = c.var.cast;
 
-  if (
-    interactor &&
-    interactor.viewerContext &&
-    interactor.viewerContext.following
-  ) {
-   
-    if (cast && cast.reactions) {
-      const { likes, recasts } = cast.reactions;
-      const interactorFid = interactor.fid;
+  if (cast && cast.hash === warpcasthash && cast.reactions) {
+    const { likes, recasts } = cast.reactions;
+    const interactorFid = interactor?.fid;
 
-      if (likes.some(like => like.fid === interactorFid) || recasts.some(recast => recast.fid === interactorFid)) {
-        return c.res({
-          action: '/Done',
-          image: 'https://based-launch.vercel.app/Youhavejoined.jpg',
-        });
-      }
+    if (likes.some(like => like.fid === interactorFid) || recasts.some(recast => recast.fid === interactorFid)) {
+      console.log("Interactor has liked or recast with the correct warpcast hash.");
+      return c.res({
+        action: '/Done',
+        image: 'https://based-launch.vercel.app/Youhavejoined.jpg',
+        intents: [
+          <Button.Link href='https://based-launch.vercel.app/api/start'>Share</Button.Link>,
+        ],
+      });
     }
-
-    return c.res({
-      action: '/Done',
-      image: "https://based-launch.vercel.app/Youhavejoined.jpg",
-      intents: [
-        <Button value="Share">Share</Button>,
-      ],
-    });
-  } else {
-   
-    return c.res({
-      action: '/Join-Waitlist',
-      image: 'https://based-launch.vercel.app/Tryagain.jpg',
-      intents: [
-        <Button.Reset>Try-again</Button.Reset>,
-        <Button.Link href="https://warpcast.com/based-launch">Follow /basedlaunch</Button.Link>,
-        <Button.Link href="https://warpcast.com/~/channel/basedlaunch">Follow @based-launch</Button.Link>,
-      ],
-    });
   }
-});
 
-app.frame('/Done', (c) => {
+  console.log("Interactor does not meet the criteria.");
   return c.res({
-    image: "https://based-launch.vercel.app/Youhavejoined.jpg",
+    action: '/Join-Waitlist',
+    image: 'https://based-launch.vercel.app/Tryagain.jpg',
     intents: [
-      <Button value="Share">Share</Button>,
+      <Button.Reset>Try-again</Button.Reset>,
+      <Button.Link href="https://warpcast.com/based-launch">Follow /basedlaunch</Button.Link>,
+      <Button.Link href="https://warpcast.com/~/channel/basedlaunch">Follow @based-launch</Button.Link>,
     ],
   });
 });
